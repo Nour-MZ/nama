ed639273e282f23df1e97bb6e514ff0d2d7ad6f5
Nour Mezher <nourmezher5@gmail.com>
2024-11-28 15:58:47 +0200

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+console.log("test")
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+   
+  
+   
+  });
