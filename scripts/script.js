a1accfe2d0e4769cf2af9aa5a9156013594f768f
Nour Mezher <nourmezher5@gmail.com>
2024-12-02 10:00:02 +0200

    finisehd desktop

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,25 @@
-console.log("test")
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
     direction: 'vertical',
     loop: true,
   
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable: 'true'
     },
-  
-   
-  
-   
   });
+
+//Landing Page section
+$('.important-links-container').click(function () {
+  // $("html, body").animate({ scrollTop: 0 }, "slow");
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+  $(this).toggleClass("active");
+  $('.important-links-overlay').toggleClass("active")
+
+});
+
+  // Events Section
+$('.events-card').click(function () {
+  $('.events-card').removeClass("active")
+  $(this).addClass("active");
+
+});
