c09163f6a39ab107dcf488f7da97b7a6e8145cd4
Nour Mezher <nourmezher5@gmail.com>
2024-12-02 17:17:16 +0200

    finisehd animations

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,20 @@
     landingtimeline.to(".swiper-pagination", { opacity: "1", duration: 0.5 }, "<"); // Same start time
     landingtimeline.to(".important-links-container", { opacity: "1", duration: 0.5 }, "<");
   
+
+    const linktl = gsap.timeline({ paused: true });
+
+    linktl.to('.important-links-button', { duration: 1, delay: 1.5, rotation: 360, ease: "power1.inOut" })
+    .to('.important-links-overlay-circle', { opacity: 1, duration: 1, ease: "power1.inOut" }, "<0.2")
+    .to('.important-links-overlay', { duration: 0.5, backgroundSize: '43rem', ease: "power1.inOut",  }, "<")
+    .to('.important-links-item:nth-child(1)', {y:"0%", x:"50%", duration: 0.5, ease: "power1.inOut",  }, "<0.2")
+    .to('.important-links-item:nth-child(2)', {y:"-0%", x:"50%", duration: 0.5, ease: "power1.inOut",  }, "<0.2")
+    .to('.important-links-item:nth-child(3)', {y:"-50%", x:"50%", duration: 0.5, ease: "power1.inOut",  }, "<0.2")
+    .to('.important-links-item:nth-child(4)', {y:"-0%", x:"-50%", duration: 0.5, ease: "power1.inOut",  }, "<0.2")
+ 
+
+
+
     ScrollTrigger.batch(".services-card", {
       once: true,
       start: "top 95%",
@@ -94,18 +108,24 @@
     });
 
   //Landing Page section
-  $('.important-links-container').click(function () {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    $(this).toggleClass("active");
-    $('.important-links-overlay').toggleClass("active")
 
-  });
+  $('.important-links-container').click(function () {
+    // Check if the clicked element doesn't have the 'active' class
+    if (!$(this).hasClass("active")) {
+        $("body").addClass('active')
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        $(this).addClass("active");
+        $('.important-links-overlay').addClass("active");
+        linktl.restart();
+    }
+});
 
 
   $('.important-links-x').click(function () {
     window.scrollTo({ top: 0, behavior: 'smooth' });
     $('.important-links-container').removeClass("active");
     $('.important-links-overlay').removeClass("active")
+    $("body").removeClass("active")
 
   });
     // Events Section
@@ -115,6 +135,27 @@
 
   });
 
+  function handleFooterToggle() {
+    if ($(window).width() > 1200) { 
+       
+        $(".footer-link-container-title").removeAttr("data-bs-toggle");
+       
+        $(".footer-link-container-title ").next('.footer-overflow-container').removeClass('accordion-collapse collapse');
+       $(".footer-overflow-container").addClass("show");
+    } else {
+        $(".footer-overflow-container").removeClass("show");
+        $(".footer-overflow-container").addClass("collapse");
+        $(".footer-header").attr("data-bs-toggle", "collapse");
+    }
+}
+
+// Initial check
+handleFooterToggle();
+
+// Run the function on window resize
+  $(window).resize(function() {
+      handleFooterToggle();
+  });
     
    })
    
