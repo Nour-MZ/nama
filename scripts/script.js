c628994bd2d480f18bacf408dab57e38097a07dd
Nour Mezher <nourmezher5@gmail.com>
2024-12-02 10:42:00 +0200

    finisehd desktop

--- scripts/script.js
+++ scripts/script.js
@@ -10,13 +10,19 @@ const swiper = new Swiper('.swiper', {
 
 //Landing Page section
 $('.important-links-container').click(function () {
-  // $("html, body").animate({ scrollTop: 0 }, "slow");
   window.scrollTo({ top: 0, behavior: 'smooth' });
   $(this).toggleClass("active");
   $('.important-links-overlay').toggleClass("active")
 
 });
 
+
+$('.important-links-x').click(function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+  $('.important-links-container').removeClass("active");
+  $('.important-links-overlay').removeClass("active")
+
+});
   // Events Section
 $('.events-card').click(function () {
   $('.events-card').removeClass("active")
